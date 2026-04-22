42eaac5318fa4ddd48ae4fd8126860e3fe77550d
mueedhussain7 <mueedhussain7@gmail.com>
2026-04-22 19:54:40 +0200

    Both DHT22 on IO13 and MQ135 on IO36 working

--- firmware/air_quality_monitor/air_quality_monitor.ino
+++ firmware/air_quality_monitor/air_quality_monitor.ino
@@ -2,21 +2,30 @@
 
 DHTesp dht;
 
+#define MQ135_PIN 36
+
 void setup() {
   Serial.begin(115200);
   delay(2000);
   dht.setup(13, DHTesp::DHT22);
-  Serial.println("Testing DHT22...");
+  Serial.println("Testing sensors...");
 }
 
 void loop() {
   delay(3000);
+  
+  // DHT22 readings
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
-  Serial.print("Status: ");
-  Serial.println(dht.getStatusString());
+  
+  // MQ135 reading
+  int gasValue = analogRead(MQ135_PIN);
+  
+  Serial.println("-------------------");
   Serial.print("Temperature: ");
   Serial.println(temperature);
   Serial.print("Humidity: ");
   Serial.println(humidity);
+  Serial.print("Gas Value: ");
+  Serial.println(gasValue);
 }
